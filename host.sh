0adbe70b1271ecf1f8bc3e9cf39ca08d018b709a
Zhaocheng Che <czc@Zhaochengs-MacBook-Pro.local>
2022-08-27 22:39:14 -0400

    Refactor hosting script

---
+++ host.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PORT=8080
+BIND=0.0.0.0
+REMOTE=localhost
+
+############################################################
+## Read in PORT and REMOTE
+
+read -p "PORT = " PORT_RDVAL
+if [[ ${PORT_RDVAL} != '' ]]; then
+	PORT=${PORT_RDVAL}
+fi
+
+read -p "REMOTE = " REMOTE_RDVAL
+if [[ ${REMOTE_RDVAL} != '' ]]; then
+	REMOTE=${REMOTE_RDVAL}
+fi
+
+############################################################
+## Install Theme
+
+if [[ ! -d themes ]]; then
+	mkdir themes
+	pushd themes
+	git clone https://github.com/athul/archie.git
+	popd themes
+	
+fi
+
+
+############################################################
+## config.toml.pub -> config.toml
+
+function configTomlGen {
+	sed "s/%REMOTE/\"${REMOTE}\"/g" config.toml.pub > config.toml
+}
+
+configTomlGen
+
+############################################################
+## Start hosting
+
+hugo server -D --bind $BIND -b $REMOTE -p $PORT
+
